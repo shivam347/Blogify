fe361cadf32cf7fd8ae07eaa7e5d6bbce9c5af10
Shivam <shivamfeb77@gmail.com>
2025-07-16 11:27:54 +0530

    Populate the data for authority check

--- src/main/java/com/example/SpringStarter/Config/SeedData.java
+++ src/main/java/com/example/SpringStarter/Config/SeedData.java
@@ -7,9 +7,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.example.SpringStarter.Models.Account;
+import com.example.SpringStarter.Models.Authority;
 import com.example.SpringStarter.Models.Post;
 import com.example.SpringStarter.Service.AccountService;
+import com.example.SpringStarter.Service.AuthorityService;
 import com.example.SpringStarter.Service.PostService;
+import com.example.SpringStarter.Util.Constants.Privileges;
 
 /**
  * SeedData class is used to populate the database with initial data
@@ -25,7 +28,10 @@ public class SeedData implements CommandLineRunner {
 
     // Inject AccountService to manage Account entities
     @Autowired
-    private AccountService accountService;  
+    private AccountService accountService;
+    
+    @Autowired
+    private AuthorityService authorityService;
 
     /**
      * This method runs at application startup and seeds the database
@@ -33,6 +39,24 @@ public class SeedData implements CommandLineRunner {
      */
     @Override
     public void run(String... args) throws Exception {
+
+        for(Privileges auth : Privileges.values()){
+            Authority authority = new Authority(null, null);
+            authority.setId(auth.getAuthorityId());
+            authority.setAuthorityName(auth.getAuthorityName());
+            authorityService.save(authority);
+        }
+
+
+
+
+
+
+
+
+
+
+
         // Create two sample Account objects
         Account account01 = new Account();
         Account account02 = new Account();
