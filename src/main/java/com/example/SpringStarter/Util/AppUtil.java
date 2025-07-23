7584b95f06c2c05c173b726c68e571935e527b51
Shivam <shivamfeb77@gmail.com>
2025-07-23 20:48:57 +0530

    change relative path to absolute path

---
+++ src/main/java/com/example/SpringStarter/Util/AppUtil.java
@@ -0,0 +1,12 @@
+package com.example.SpringStarter.Util;
+
+import java.io.File;
+
+public class AppUtil {
+
+    public static String get_upload_path(String filename){
+
+        return new File("src\\main\\resources\\static\\uploads").getAbsolutePath()+"\\" + filename;
+    }
+    
+}
