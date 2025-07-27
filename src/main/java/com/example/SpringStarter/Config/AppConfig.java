f14b2d3f71b5ea62ff7c151797d78960f7553622
Shivam <shivamfeb77@gmail.com>
2025-07-27 15:48:37 +0530

    configuration file for app config

---
+++ src/main/java/com/example/SpringStarter/Config/AppConfig.java
@@ -0,0 +1,59 @@
+package com.example.SpringStarter.Config;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class AppConfig {
+
+    @Value("${spring.mail.host}")
+    private String mailHost;
+
+    @Value("${spring.mail.port}")
+    private String mailPort;
+
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+
+    @Value("${spring.mail.password}")
+    private String mailPassword;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private String smtpAuth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private String starttlsEnable;
+
+    @Value("${mail.transport.protocol}")
+    private String mailTransportProtocol;
+
+    @Value("${spring.mail.properties.mail.debug}")
+    private String mailDebug;
+
+    @Value("${spring.mail.smtp.ssl.trust}")
+    private String sslTrust;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(mailHost);
+        mailSender.setPort(Integer.parseInt(mailPort));
+        mailSender.setUsername(mailUsername);
+        mailSender.setPassword(mailPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", mailTransportProtocol);
+        props.put("mail.smtp.auth", smtpAuth);
+        props.put("mail.smtp.starttls.enable", starttlsEnable);
+        props.put("mail.debug", mailDebug);
+        props.put("mail.smtp.ssl.trust", sslTrust);
+
+        return mailSender;
+    }
+}
