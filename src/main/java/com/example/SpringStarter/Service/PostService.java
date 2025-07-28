1bf17f1eb88a7783d4aa4dab8e67beec0f7a15d0
Shivam <shivamfeb77@gmail.com>
2025-07-28 14:19:08 +0530

    updated findall method for pagination

--- src/main/java/com/example/SpringStarter/Service/PostService.java
+++ src/main/java/com/example/SpringStarter/Service/PostService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.example.SpringStarter.Models.Post;
@@ -23,8 +26,14 @@ public class PostService {
     }
 
     // to get all the posts
-    public List<Post> getAll(){
+    public List<Post> findAll(){
         return postRepository.findAll();
+    }
+
+
+    public Page<Post> findAll(int offset, int pageSize, String field){
+        PageRequest pageRequest = PageRequest.of(offset, pageSize, Sort.by(Sort.Direction.ASC, field));
+        return postRepository.findAll(pageRequest);
     }
 
     // to delete the post
@@ -35,10 +44,10 @@ public class PostService {
     // to save the post
     public Post save(Post post){
         if(post.getId() == null){
-            post.setPostedOn(LocalDateTime.now());
+            post.setCreatedAt(LocalDateTime.now());
         }
 
-        post.setUpdatedOn(LocalDateTime.now());
+        post.setUpdatedAt(LocalDateTime.now());
 
         return postRepository.save(post);
     }
